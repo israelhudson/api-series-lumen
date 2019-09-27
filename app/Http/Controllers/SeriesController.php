b431e9ca9eea4b9763cd24e9aef7f1b0ac013d8f
israelhudson <israelhudson@gmail.com>
2019-09-26 23:37:56 -0300

    criando banco de dados com SQLITE e Migrations

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -1,15 +1,13 @@
 <?php
 namespace App\Http\Controllers;
 
+use App\Serie;
 use Illuminate\Http\Request;
 
 class SeriesController
 {
     public function index()
     {
-        return [
-            "Anjo mau",
-            "Farofa"
-        ];
+        return Serie::all();
     }
 }
